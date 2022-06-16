6f6052ab72f229de333bee3951ddbe2948253640
Saowanee Saewong <apple@atop.com.tw>
2022-06-16 20:23:27 +0700

    modify Examplecode

--- test2/testmain2.go
+++ test2/testmain2.go
@@ -4,5 +4,6 @@ import "fmt"
 
 func Examplecode(strinput string) string {
 	outstring := fmt.Sprintf("test2/examplecode: %s", strinput)
+	fmt.Println("Example code is running")
 	return outstring
 }
