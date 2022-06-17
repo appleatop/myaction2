5c0373b49c7217a7019106ddd3b69b5d6d275e2d
Saowanee Saewong <apple@atop.com.tw>
2022-06-17 18:35:56 +0700

    change test code

--- test2/testmain2_test.go
+++ test2/testmain2_test.go
@@ -21,9 +21,9 @@ func TestProgram2(t *testing.T) {
 func TestExampleCode(t *testing.T) {
 	fmt.Println("testing testprogram 2/ example code")
 	q.Q("testing testprogram 2/ example code ")
-	//output := test2.Examplecode("test pass")
-	output := test2.Examplecode("test error")
-	expect := "test2/examplecode: test error"
+	output := test2.Examplecode("test pass")
+	//output := test2.Examplecode("test error")
+	expect := "test2/examplecode: test pass"
 	if output != expect {
 		t.Errorf("The result (%s) does not match %s", output, expect)
 	} else {
